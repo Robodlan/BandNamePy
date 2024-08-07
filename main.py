1e220fe84934b3cf3c32eb6dbb9d9bf5ae0429e6
Robodlan <azuldevs@zohomail.com>
2024-08-07 09:29:14 -0400

    Space

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print("Welcome to the Band Name Generator")
 city = input("What's the name of the city you grew up in?\n")
 pet = input("What is your pet name?\n")
-print("You band name is\n" + city + pet )
+print("You band name is\n " + city + " " + pet )
